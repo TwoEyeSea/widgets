713a4de9e980f56fd35c198640e45c5049c60474
M Alexander Moroney <alexmoroney@gmail.com>
2022-08-02 18:28:08 -0500

    L143 - Expanding the Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,32 +1,29 @@
 import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(null); //As soon as we call the setter function once the default value is no longer used until the page is refreshed
 
   const onTitleClick = (index) => {
     setActiveIndex(index);
+    //calling the setter function to update the useState.
   };
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(index)}>
-          <i className="dropdown icon"></i>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          3<i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
