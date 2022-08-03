f1a6252e0920fe8602ab73917e0bca601ce5122b
M Alexander Moroney <alexmoroney@gmail.com>
2022-08-02 22:34:12 -0500

    L150 through to 153 - learning how to use the useEffect( )

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
 
+  useEffect(() => {
+    // OPTION 1 - DEFINING A TEMPORARY FUNCTION TO USE ASYNC/AWAIT:
+    const searchWiki = async () => {
+      await axios.get();
+    };
+
+    // OPTION 2 - REMOVING THE TEMPORARY FUNCTION:
+    //   (async () => {
+    //   await axios.get("yada");
+    // })();
+
+    //OPTION 3 - USING PROMISES:
+    //  axios.get("yada")
+    //  .then((repose) => {console.log(response.data);
+    //  });
+
+    //USING AXIOS WITH useEffect
+  }, [term]);
+  // The first argument for useEffect() function is the function we want to run, the second argument determines which of the 3 cases to utilize for our useEffect().
+  // See section 12 L151 for options and outcomes.
   return (
     <div>
       <div className="ui form">
@@ -11,6 +32,7 @@ const Search = () => {
           <input
             value={term}
             onChange={(e) => setTerm(e.target.value)}
+            T
             className="inut"
             placeholder="Please enter a search term :)"
           />
